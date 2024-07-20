5a000956304f9487785b6ea9fc5a7b81b9d8e8ef
Stanislław Jakubowski <sjakubowski3554@gmail.com>
2024-07-20 14:06:38 +0200

    Finish

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use rand::random;
+use std::{thread, time};
 
 struct Board {
     size: usize,
@@ -19,10 +20,106 @@ impl Board {
         }
     }
 
-    fn get_cell(&self, x: usize, y: usize) -> bool {
+    fn get_cell(&self, x: usize, y: usize) -> Option<&bool> {
         let row = self.get_row(y);
 
-        row[x]
+        row.get(x)
+    }
+
+    fn get_num_of_alive_neighbours(&self, x: usize, y: usize) -> usize {
+        let mut neighbours: Vec<&bool> = Vec::new();
+
+        // left top diagonal
+        if x != 0 && y != 0 {
+            if let Some(state) = self.get_cell(x - 1, y - 1) {
+                if state.to_owned() {
+                    neighbours.push(state)
+                }
+            }
+        }
+
+        // top
+        if y != 0 {
+            if let Some(state) = self.get_cell(x, y - 1) {
+                if state.to_owned() {
+                    neighbours.push(state)
+                }
+            }
+        }
+
+        // right top diagonal
+        if y != 0 && x != self.size - 1 {
+            if let Some(state) = self.get_cell(x + 1, y - 1) {
+                if state.to_owned() {
+                    neighbours.push(state)
+                }
+            }
+        }
+
+        // left
+        if x != 0 {
+            if let Some(state) = self.get_cell(x - 1, y) {
+                if state.to_owned() {
+                    neighbours.push(state)
+                }
+            }
+        }
+
+        // right
+        if x != self.size - 1 {
+            if let Some(state) = self.get_cell(x + 1, y) {
+                if state.to_owned() {
+                    neighbours.push(state)
+                }
+            }
+        }
+
+        // left bottom diagonal
+        if y != self.size - 1 && x != 0 {
+            if let Some(state) = self.get_cell(x - 1, y + 1) {
+                if state.to_owned() {
+                    neighbours.push(state)
+                }
+            }
+        }
+
+        // bottom
+        if y != self.size - 1 {
+            if let Some(state) = self.get_cell(x, y + 1) {
+                if state.to_owned() {
+                    neighbours.push(state)
+                }
+            }
+        }
+
+        // right bottom diagonal
+        if y != self.size - 1 && x != self.size - 1 {
+            if let Some(state) = self.get_cell(x + 1, y + 1) {
+                if state.to_owned() {
+                    neighbours.push(state)
+                }
+            }
+        }
+
+        neighbours.len()
+    }
+
+    fn create_next_generation(&mut self) {
+        let mut new_cells = self.cells.clone();
+
+        for x in 0..self.size {
+            for y in 0..self.size {
+                let n = self.get_num_of_alive_neighbours(x, y);
+                let index = y * self.size + x;
+
+                new_cells[index] = match self.cells[index] {
+                    true => n == 2 || n == 3,
+                    false => n == 3,
+                };
+            }
+        }
+
+        self.cells = new_cells;
     }
 
     fn get_row(&self, index: usize) -> &[bool] {
@@ -40,15 +137,19 @@ impl Board {
 
             println!("{}", row.join(""))
         }
+
+        println!();
     }
 }
 
 fn main() {
-    let board = Board::new(30);
+    let mut board = Board::new(45);
 
-    board.print();
+    loop {
+        board.print();
+        board.create_next_generation();
 
-    let coord = board.get_cell(0, 0);
-
-    println!("{}", coord)
+        let ten_millis = time::Duration::from_millis(10);
+        thread::sleep(ten_millis);
+    }
 }
