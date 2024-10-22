b80d7737016ee912eb40f3b749584910fc69c008
Stanislław Jakubowski <sjakubowski3554@gmail.com>
2024-10-22 21:27:30 +0200

    First c

--- c/main.c
+++ c/main.c
@@ -1,3 +1,25 @@
-int main() {
-    return 0;
+#include <stdbool.h>
+#include <stdlib.h>
+
+struct Board {
+  int size;
+  bool cells[45];
+  bool next[45];
+};
+
+struct Board createBoard(int size) {
+  struct Board b;
+  b.size = size;
+
+  for (int i = 0; i < size * size; i++) {
+    b.cells[i] = rand() % 2;
+  }
+
+  for (int i = 0; i < size * size; i++) {
+    b.next[i] = false;
+  }
+
+  return b;
 }
+
+int main() { return 0; }
